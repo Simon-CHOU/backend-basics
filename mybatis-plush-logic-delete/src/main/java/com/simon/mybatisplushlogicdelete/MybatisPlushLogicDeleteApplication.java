9b42e1e7e5b97ae2fd6bbcb47ecf550998d0483d
Simon-CHOU <mrsimonzhou@gmail.com>
2021-08-02 11:46:29 +0800

    learn logic delete

---
+++ mybatis-plush-logic-delete/src/main/java/com/simon/mybatisplushlogicdelete/MybatisPlushLogicDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.simon.mybatisplushlogicdelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlushLogicDeleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlushLogicDeleteApplication.class, args);
+	}
+
+}
---
+++ mybatis-plush-logic-delete/src/test/java/com/simon/mybatisplushlogicdelete/MybatisPlushLogicDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.mybatisplushlogicdelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlushLogicDeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
