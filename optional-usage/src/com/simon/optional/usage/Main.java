a9755310aac8bbd29de9294d8afc8411a461d503
Simon-CHOU <mrsimonzhou@gmail.com>
2021-09-24 14:58:53 +0800

    Creating an Optional

--- optional-usage/src/com/simon/optional/usage/Main.java
+++ optional-usage/src/com/simon/optional/usage/Main.java
@@ -1,6 +1,9 @@
 package com.simon.optional.usage;
 
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
@@ -14,5 +17,19 @@ public class Main {
         optional.get().incrementAndGet(); //Retrieve contained value and increment
         System.out.println(optional); //Optional[2]
         optional = Optional.ofNullable(new AtomicInteger()); //Optional reference can be reassigned
+    }
+
+    public static <T> Optional<T> createOptionalTheHardWay(T value) {
+        return value == null ? Optional.empty() : Optional.of(value);
+    }
+
+    public static <T> Optional<T> createOptionalTheEasyWay(T value) {
+        return Optional.ofNullable(value);
+    }
+
+    public void createPrimitiveOptional(){
+        OptionalInt optionalInt = OptionalInt.of(1);
+        OptionalLong optionalLong = OptionalLong.of(1L);
+        OptionalDouble optionalDouble = OptionalDouble.of(1.0);
     }
 }
