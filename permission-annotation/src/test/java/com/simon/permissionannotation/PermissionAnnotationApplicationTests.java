851448c84c4fb794636960d2a62d1d22b37b1ae1
Simon-CHOU <mrsimonzhou@gmail.com>
2022-02-28 00:42:43 +0800

    init permission-annotation project

---
+++ permission-annotation/src/test/java/com/simon/permissionannotation/PermissionAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.permissionannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
