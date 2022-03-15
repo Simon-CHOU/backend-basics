269e4362d4bb2bf61816a590c0980af1bd869753
Simon-CHOU <mrsimonzhou@gmail.com>
2022-03-15 12:29:10 +0800

    okta junit5 dev demo

---
+++ okta-junit5-tutorial/src/main/java/com/simon/oktajunit5tutorial/OktaJunit5TutorialApplication.java
@@ -0,0 +1,13 @@
+package com.simon.oktajunit5tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaJunit5TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaJunit5TutorialApplication.class, args);
+	}
+
+}
---
+++ okta-junit5-tutorial/src/test/java/com/simon/oktajunit5tutorial/OktaJunit5TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.oktajunit5tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaJunit5TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
