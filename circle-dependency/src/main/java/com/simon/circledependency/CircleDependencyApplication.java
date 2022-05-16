d8db1fff6186e4faa91f55f30089fb42c8fe03dd
Simon-CHOU <mrsimonzhou@gmail.com>
2022-05-16 10:19:22 +0800

    Circle Dependency: init

---
+++ circle-dependency/src/main/java/com/simon/circledependency/CircleDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.simon.circledependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleDependencyApplication.class, args);
+	}
+
+}
---
+++ circle-dependency/src/test/java/com/simon/circledependency/CircleDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.circledependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleDependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
