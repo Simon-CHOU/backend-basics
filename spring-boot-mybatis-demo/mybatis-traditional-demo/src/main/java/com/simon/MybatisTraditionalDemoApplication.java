0a521a5ea8f993f0bfe6eb090f90bff1524d156e
Simon-CHOU <mrsimonzhou@gmail.com>
2022-06-08 01:13:11 +0800

    learn mybatis& mybatis plus
    learn and compare

---
+++ spring-boot-mybatis-demo/mybatis-traditional-demo/src/main/java/com/simon/MybatisTraditionalDemoApplication.java
@@ -0,0 +1,11 @@
+package com.simon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisTraditionalDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisTraditionalDemoApplication.class, args);
+    }
+}
