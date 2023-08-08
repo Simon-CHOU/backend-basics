ea93532718f6e417543922074f164d5b69a8b858
Simon-CHOU <mrsimonzhou@gmail.com>
2023-08-08 22:17:47 +0800

    Dockefile practice: init

---
+++ dockerfile-practice/src/main/java/com/simon/dockerfilepractice/DockerfilePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.simon.dockerfilepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerfilePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerfilePracticeApplication.class, args);
+	}
+
+}
---
+++ dockerfile-practice/src/test/java/com/simon/dockerfilepractice/DockerfilePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.dockerfilepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerfilePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
