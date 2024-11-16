d7156aac3e99b13c753327ce4c9e525c41a81c84
Simon-CHOU <mrsimonzhou@gmail.com>
2024-11-17 01:06:31 +0800

    Spring Boot integrate RabbitMQ init

---
+++ rabbitmq-firstlook/src/main/java/com/simon/rabbitmq_firstlook/RabbitmqFirstlookApplication.java
@@ -0,0 +1,13 @@
+package com.simon.rabbitmq_firstlook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqFirstlookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqFirstlookApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-firstlook/src/test/java/com/simon/rabbitmq_firstlook/RabbitmqFirstlookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.rabbitmq_firstlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqFirstlookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
