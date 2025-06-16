41b140b9ab5f133b03f6f09590216f1d5045dc30
Simon-CHOU <mrsimonzhou@gmail.com>
2025-06-16 23:37:39 +0800

    jpa play: 创建student dto

---
+++ mastering-spring-data-jpa/src/main/java/dao/Student.java
@@ -0,0 +1,38 @@
+package dao;
+
+import convertor.GenderConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Converter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Student {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "age")
+    private String age;
+    @Column(name = "gender" )
+    @Convert(converter = GenderConverter.class)  // 关键修改：将 @Converter 改为 @Convert
+    private String gender;
+}
