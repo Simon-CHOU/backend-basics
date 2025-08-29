6e3d7fcaa7fc693dd1d93acf5f160a1433098d52
Simon-CHOU <mrsimonzhou@gmail.com>
2025-08-29 12:33:02 +0800

    labs020: fix deprecated swagger api

--- interview-complete/lab020/src/main/java/com/simon/lab020/config/SecurityConfig.java
+++ interview-complete/lab020/src/main/java/com/simon/lab020/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -44,7 +45,7 @@ public class SecurityConfig {
             
             // Allow frames for H2 console
             .headers(headers -> headers
-                .frameOptions().sameOrigin()
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
             );
 
         return http.build();
